f9cb985504062ab3f91d328259c8671b0fbb2925
svrs <elisacueille1109@gmail.com>
2024-11-22 10:12:06 +0100

    day 6 1 2020

---
+++ adventOfCode/2022/day6-1.py
@@ -0,0 +1,19 @@
+import re
+
+def lire(nom: str):
+
+    with open(nom, encoding="UTF-8", mode= "r") as file:  
+            lignes = file.readlines()
+    return lignes
+
+
+
+# recuperation ligne
+ligne = lire('adventOfCode/2022/input.txt')[0]
+print(ligne)
+
+for i in range(3,len(ligne) +1):
+    chaine = ligne[i-3:i+1]
+    if len(chaine) == len(set(chaine)):
+     print(i+1)
+     break
