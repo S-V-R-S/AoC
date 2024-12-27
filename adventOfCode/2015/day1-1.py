d8c7a40d746bb659e8cc53d13dc12ad3eee2b5ba
svrs <elisacueille1109@gmail.com>
2024-12-27 23:23:11 +0100

    day 1 2015

--- adventOfCode/2015/day1-1.py
+++ adventOfCode/2015/day1-1.py
@@ -1,15 +1,15 @@
-print("good start")
 
-# OUVERTURE DE L INPUT 
-f = open("adventOfCode\\2015\\input.txt","r")
-ligne = f.read().strip()
-f.close()
+with open('adventOfCode/2015/input.txt', encoding="UTF-8", mode= "r") as file:  
+    line = file.read()
 
-# ON COMMENCE A L ETAGE 0 
-etage = 0
-for c in ligne:
-    # ACTUALISATION DE LA VALEUR DE L ETAGE SELON LE SENS DE LA PARENTHESE 
-    if c =='(': etage += 1
-    else: etage -= 1
+ 
+floor = 0
 
-print('Réponse:', etage)
+for parenthesis in line:
+    if parenthesis == "(":
+        floor += 1
+    else:
+        floor -= 1
+        
+        
+print(floor)
--- adventOfCode/2015/day1-2.py
+++ adventOfCode/2015/day1-2.py
@@ -1,24 +1,16 @@
-print("good start")
 
-# OUVERTURE DE L INPUT 
-f = open("adventOfCode\\2015\\input.txt","r")
-ligne = f.read().strip()
-f.close()
+with open('adventOfCode/2015/input.txt', encoding="UTF-8", mode= "r") as file:  
+    line = file.read()
 
-# ON COMMENCE A L ETAGE 0 
-etage = 0
-# ON INIALISE LE COMPTEUR 
 position = 0
+floor = 0
 
-# TANT QU ON N EST PAS AU -1
-while etage != -1:
-
-    # ON ACTUALISE L ETAGE 
-    c = ligne[position]
-    if c =='(': etage += 1
-    else: etage -= 1
-
-    # ON INCREMENTE LE COMPTEUR 
+for parenthesis in line:
     position += 1
-
-print('Réponse:', position)
+    if parenthesis == "(":
+        floor += 1
+    else:
+        floor -= 1
+    if floor == -1:
+        print(position)
+        break
