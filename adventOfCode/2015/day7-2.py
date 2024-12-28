ee9da634d4c6c313ad305824cac3c75d0fd437e3
svrs <elisacueille1109@gmail.com>
2024-12-28 15:28:38 +0100

    day 7 2015

--- adventOfCode/2015/day7-2.py
+++ adventOfCode/2015/day7-2.py
@@ -1,10 +1,54 @@
+import re
+with open('adventOfCode/2015/input.txt', encoding="UTF-8", mode= "r") as file:  
+    gates = file.readlines()
+    
+wires = {}
+wires["b"] = 956
+for gate in gates:
+    gate = gate.strip()
+    
+    if gate.startswith("NOT"):
+        _,x,y = gate.replace("-> ", "").split(" ")
+        if x in wires:
+            wires[y] =(~wires[x] & 0xFFFF)
+        else:
+            gates.append(gate)
+            
+    elif "AND" in gate or "OR" in gate or "LSHIFT" in gate or "RSHIFT" in gate:
+        x,_,y,z = gate.replace("-> ", "").split(" ")
+        
+        if x.isdigit(): x = int(x)
+        elif x not in wires: 
+            gates.append(gate)
+            continue
+        else: x = wires[x]
+        
+        if y.isdigit(): y = int(y)
+        elif y not in wires: 
+            gates.append(gate)
+            continue
+        else: y = wires[y]
+        
+        if "AND" in gate:
+            wires[z] = x & y
+        elif "OR" in gate:
+            wires[z] = x | y
+        elif "RSHIFT" in gate:
+            wires[z] = x >> y
+        elif "LSHIFT" in gate:
+            wires[z] = x << y
+    
+    else:
+        value, wire = gate.split(" -> ")
+        if wire == "b": continue
+        if value.isdigit():
+            wires[wire] = int(value)
+        elif value not in wires:
+            gates.append(gate)
+            continue
+        else:
+            wires[wire] = wires[value]
+        
 
-import time
-start_time = time.time()
-
-
-
-
-end_time = time.time()
-elapsed_time_ms = (end_time - start_time) * 1000
-print(f"Le programme a dure {elapsed_time_ms:.2f} ms est la reponse est")
+print(wires['a'])   
+    
