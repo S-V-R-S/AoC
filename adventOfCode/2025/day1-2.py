9f9cd950f283a3704334c560b107c02a0ba875fc
Elisa CUEILLE <elisacueille1109@gmail.com>
2025-12-01 13:49:09 +0100

    cleaner

--- adventOfCode/2025/day1-2.py
+++ adventOfCode/2025/day1-2.py
@@ -5,53 +5,45 @@ start_time = time.time()
 import re
 import os
 
-def creer_matrice():
-    with open('adventOfCode/2024/input.txt', encoding="UTF-8", mode= "r") as file:  
-        lignes = file.read().splitlines()
-        matrice = []
-        for i, ligne in enumerate(lignes):
-            matrice.append([])
-            for j, letter in enumerate(ligne):
-                matrice[i].append(letter)
-    return matrice
 
-# a, b = int(re.findall("\d+", ligne)[0]), int(re.findall("\d+", ligne)[1])
+
 with open('adventOfCode/2025/input1.txt', encoding="UTF-8", mode= "r") as file:  
 # with open('adventOfCode/2025/test.txt', encoding="UTF-8", mode= "r") as file:  
         lignes = file.read().splitlines()
 
-
-max = 99
 compteur = 0
 pointeur = 50 
 
-
 for ligne in lignes:
-    lettre = ligne[0]
-    nbr = int(ligne[1:])
-     
-    if lettre == "L":
-        while nbr > pointeur:
-            nbr = nbr - pointeur - 1
-            if pointeur != 0:
-                 compteur += 1
-            pointeur = 99
-        pointeur = pointeur - nbr
-        if pointeur == 0:
-            compteur += 1  
-    else : 
-        while nbr + pointeur > 99:
-              nbr = nbr - (99 - pointeur + 1) 
-              pointeur = 0
-              compteur +=1
-        pointeur = pointeur + nbr
-    print(pointeur, compteur)
+        direction = ligne[0]
+        nombre = int(ligne[1:])
+        
+        if direction == "L":
+                while nombre > pointeur:
+                        nombre = nombre - pointeur - 1
+                        if pointeur != 0:
+                                compteur +=1
+                        pointeur = 99
 
-print(compteur)
+                pointeur = pointeur - nombre
+                if pointeur == 0:
+                       compteur += 1
+        else:
+                while nombre + pointeur > 99:
+                        nombre = nombre - (99 - pointeur + 1)
+                        compteur +=1
+                        pointeur = 0
+                        
+                pointeur = pointeur + nombre
         
-        
+print(compteur)       
+
+end_time = time.time()
+elapsed_time_ms = (end_time - start_time) * 1000
+print(f"Le programme a dure {elapsed_time_ms:.2f} ms est la reponse est")
+
                 
-
+# 5961
 
 
 
