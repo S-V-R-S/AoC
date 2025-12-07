22e5cc2955d3c0bb977ccf7c262c8b4f51996883
Elisa CUEILLE <elisacueille1109@gmail.com>
2025-12-07 08:22:01 +0100

    day 7/8

--- adventOfCode/2025/day7-1.py
+++ adventOfCode/2025/day7-1.py
@@ -4,7 +4,36 @@ start_time = time.time()
 
 
 
+with open('adventOfCode/2025/test.txt', encoding="UTF-8", mode= "r") as file:  
+    lignes = file.read().split("\n")
+    matrice = []
+    for i, ligne in enumerate(lignes):
+        matrice.append([])
+        for j, letter in enumerate(ligne):
+            matrice[i].append(letter)
+            if letter =="S":
+                start = [i,j]
+                matrice[i][j] = "|"
 
+division = 0
+for i in range(len(matrice)-1):
+    for j in range(len(matrice[0])):
+        if matrice[i][j] == "|":
+            print(i,j)
+            if matrice[i+1][j] == ".":
+                matrice[i+1][j] = "|"
+            elif matrice[i+1][j] == "^":
+                division += 1
+                if matrice[i+1][j-1] == ".":
+                    matrice[i+1][j-1] = "|"
+                if matrice[i+1][j+1] == ".":
+                    matrice[i+1][j+1] = "|"
+
+print(division)
+
+
+        
 end_time = time.time()
 elapsed_time_ms = (end_time - start_time) * 1000
 print(f"Le programme a dure {elapsed_time_ms:.2f} ms est la reponse est")
+# 13 minutes
--- adventOfCode/2025/day7-2.py
+++ adventOfCode/2025/day7-2.py
@@ -4,7 +4,38 @@ start_time = time.time()
 
 
 
+with open('adventOfCode/2025/test.txt', encoding="UTF-8", mode= "r") as file:  
+    lignes = file.read().split("\n")
+    matrice = []
+    for i, ligne in enumerate(lignes):
+        matrice.append([])
+        for j, letter in enumerate(ligne):
+            matrice[i].append(letter)
+            if letter =="S":
+                matrice[i][j] = 1
+            if letter == ".":
+                 matrice[i][j] = 0
 
+
+for i in range(len(matrice)-1):
+    for j in range(len(matrice[0])):
+        if matrice[i][j] != "^":
+            if matrice[i+1][j] != "^":
+                matrice[i+1][j] += matrice[i][j]
+            elif matrice[i+1][j] == "^":
+                if matrice[i+1][j-1] != "^":
+                    matrice[i+1][j-1]  += matrice[i][j]
+                if matrice[i+1][j+1] != "^":
+                    matrice[i+1][j+1]  += matrice[i][j]
+
+somme = 0
+for i in matrice[-1]:
+    if i != "^":
+        somme += i
+
+print(somme)      
 end_time = time.time()
 elapsed_time_ms = (end_time - start_time) * 1000
 print(f"Le programme a dure {elapsed_time_ms:.2f} ms est la reponse est")
+# 12 minutes
+# total = 25
