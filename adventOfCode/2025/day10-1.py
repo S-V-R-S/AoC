b29a7bbe818ba5c7bfd27e54734213000c897ff6
elisa <elisacueille1109@gmail.com>
2025-12-10 22:32:09 +0100

    day 10-1

--- adventOfCode/2025/day10-1.py
+++ adventOfCode/2025/day10-1.py
@@ -1,12 +1,12 @@
-
+import itertools
 import time
 start_time = time.time()
 
 with open('adventOfCode/2025/test.txt', encoding="UTF-8", mode= "r") as file:  
         lignes = file.read().splitlines()
 
-appuie = 0
 
+total = 0
 for ligne in lignes:
         parties = ligne.split(" ")
         voyants = parties[0]
@@ -15,7 +15,38 @@ for ligne in lignes:
         print(voyants)
         print(tension)
         print(boutons)
-        departs = ["." for i in range(len(voyants))]
+        objectif = [i for i in voyants[1:-1]]
+        print(objectif)
+        
+        trouve = False
+        nbr = 1
+        while not(trouve):
+                print("nbr",nbr)
+                combinaisons = list(itertools.combinations(boutons, nbr))
+                print(combinaisons)
+                for sequence in combinaisons:
+                        depart = ["." for i in range(len(voyants)-2)]
+                        for s in sequence:
+                                btns = [int(x) for x in s.strip("()").split(",")]
+                                for btn in btns:
+                                        if depart[btn] == ".":
+                                                depart[btn] = "#"
+                                        else:
+                                                depart[btn] = "."
+                        print(depart, objectif)
+                        if depart == objectif:
+                                total += nbr
+                                trouve = True
+                                print(trouve)
+                                break
+                print(trouve)
+                if trouve:
+                        continue                
+                nbr +=1
+
+
+
+print(total)
 
 
 end_time = time.time()
