b94048288f08df2e2d0e1632803577416d4d53be
elisa <elisacueille1109@gmail.com>
2025-12-11 08:26:41 +0100

    11

--- adventOfCode/2025/day11-1.py
+++ adventOfCode/2025/day11-1.py
@@ -2,9 +2,52 @@
 import time
 start_time = time.time()
 
+with open('adventOfCode/2025/test.txt', encoding="UTF-8", mode= "r") as file:  
+        lignes = file.read().splitlines()
+
+
+
+def allerOut(dico, porte, step):
+        somme = 0
+        for s in dico[porte]:
+                if s != "out":
+                        somme += allerOut(dico, s, step +1)
+                else:
+                    somme += 1
+
+        return somme 
+        
+        
+
+
+dico = {}
+for ligne in lignes:
+        entree, sorties = ligne.split(":")
+        sortiesList =  []
+        for s in sorties.strip().split(' '):
+                sortiesList.append(s)
+        dico[entree] = sortiesList
+
+print(dico)
+print(allerOut(dico, "svr", 0))
+
+
+                
 
 
 
 end_time = time.time()
 elapsed_time_ms = (end_time - start_time) * 1000
 print(f"Le programme a dure {elapsed_time_ms:.2f} ms est la reponse est")
+
+
+# aaa: you hhh
+# you: bbb ccc
+# bbb: ddd eee
+# ccc: ddd eee fff
+# ddd: ggg
+# eee: out
+# fff: out
+# ggg: out
+# hhh: ccc fff iii
+# iii: out
--- adventOfCode/2025/day11-2.py
+++ adventOfCode/2025/day11-2.py
@@ -2,9 +2,59 @@
 import time
 start_time = time.time()
 
+with open('adventOfCode/2025/test.txt', encoding="UTF-8", mode= "r") as file:  
+        lignes = file.read().splitlines()
+
+
+dico = {}
+
+
+
+def allerOut(porte, fft, dac):
+        global dico
+        somme = 0
+        for s in dico[porte]:
+                if s != "out":
+                            if "fft" == s:
+                                fft = True
+                            if "dac" == s:
+                                dac = True
+                          
+                            somme += allerOut(s, fft, dac)
+                else:
+                       if fft and dac:
+                            somme += 1
+        return somme 
+        
+        
+
+
+for ligne in lignes:
+        entree, sorties = ligne.split(":")
+        sortiesList =  []
+        for s in sorties.strip().split(' '):
+                sortiesList.append(s)
+        dico[entree] = sortiesList
+
+print(allerOut("svr", False, False))
+
+
+                
 
 
 
 end_time = time.time()
 elapsed_time_ms = (end_time - start_time) * 1000
 print(f"Le programme a dure {elapsed_time_ms:.2f} ms est la reponse est")
+
+
+# aaa: you hhh
+# you: bbb ccc
+# bbb: ddd eee
+# ccc: ddd eee fff
+# ddd: ggg
+# eee: out
+# fff: out
+# ggg: out
+# hhh: ccc fff iii
+# iii: out
